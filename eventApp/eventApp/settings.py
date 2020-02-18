fa5c8e094a52762d6e3e6ca808f552fe05190f26
Manpreet Dhillon <manpreetdhillon@Manpreets-MacBook-Pro.local>
2020-02-18 09:46:23 -0600

    added login functionality

--- eventApp/eventApp/settings.py
+++ eventApp/eventApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eventApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,13 +74,22 @@ WSGI_APPLICATION = 'eventApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': 'man1995root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
--- eventApp/eventApp/views.py
+++ eventApp/eventApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .Models.models import Testt
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -11,7 +14,6 @@ chatbot = ChatBot(
 )
 trainer = ChatterBotCorpusTrainer(chatbot.storage)
 trainer.train("/Users/mehakluthra/Documents/Event_Management_Chatterbot/eventApp/custom_corpus/mehak.yml")
-
 # Train based on the english corpus
 
 #Already trained and it's supposed to be persistent
@@ -41,3 +43,36 @@ def get_response(request):
 def home(request, template_name="home.html"):
 	context = {'title': 'Chatbot Version 1.0'}
 	return render_to_response(template_name, context)
+
+def homee(request):
+    obj = Testt.objects.get(id=1)
+    context = {"object": obj}
+    return render(request,'login.html',context)
+   
+def login_request(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'listAllEvents.html')
+    else:
+        messages.error(request, "Invalid username or password.")
+
+def register(request):
+    " register"
+    return render(request, 'register.html')
+
+def list_all_events(request):
+    " list"
+    context = {"totalEvents": 6}
+    return render(request, 'listAllEvents.html',context)
+
+def event_detail(request):
+    "detial"
+    return render(request, 'eventDetail.html')
+
+def logout_request(request):
+    "detial"
+    logout(request)
+    return render(request, 'login.html')
